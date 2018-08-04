8f4e241d8220949e6e4ceced03113a8bfba0cdfd
Gauri Bapat <gaurib@umich.edu>
2018-08-03 20:53:11 -0400

    set up api, working on integrating it into current code
    error message: TypeError: Got an unexpected keyword argument "calendarID"

--- testREST.py
+++ testREST.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from flask import Flask, jsonify, send_from_directory, request, url_for
 import glob
 import os
@@ -5,8 +6,14 @@ from os.path import join
 from stat import S_ISREG, ST_CTIME, ST_MODE
 import sys, time
 import csv
+import datetime
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file as oauth_file, client, tools
 # from werkzeug.security import safe_join
 
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 app = Flask(__name__)
 
 
@@ -19,8 +26,27 @@ def generateGraphs():
 	if request.method == 'POST':
 		data = request.get_json();
 		print(data);
-	return app.send_static_file('index.html')
 
+		store = oauth_file.Storage('token.json');
+		creds = store.get()
+		if not creds or creds.invalid:
+			flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+			creds = tools.run_flow(flow, store)
+		service = build('calendar', 'v3', http=creds.authorize(Http()))	
+
+		#call the calendar api
+		now = datetime.datetime.utcnow().isoformat() + 'Z' # Z indicates UTC time
+		print('Getting the upcoming 10 events')
+		events_result = service.events().list(calendarID='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+		events = events_result.get('items', [])
+
+		if not events:
+			print('no upcoming events found.')
+		for event in events:
+			start = event['start'].get('datetime', event['start'].get('date'))
+			print(start, event['summary'])
+	    # creds = store.get()
+	return app.send_static_file('index.html')
 
 ##################### ERROR HANDLING ###########################################
 @app.errorhandler(500)
